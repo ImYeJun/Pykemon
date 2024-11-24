3ce27c6c618cca314c1fcc015c3d17843989c2c6
prof_abyss <yejun6210@gmail.com>
2024-11-24 15:08:12 +0900

    Feat : 게임 선택창 추가

---
+++ main.py
@@ -0,0 +1,55 @@
+import pygame
+
+pygame.init()
+
+size_x = 500
+size_y = 500
+running = True  
+
+screen = pygame.display.set_mode((size_x,size_y))
+pygame.display.set_caption("pykemon")
+
+fontPath = "Pykemon/assets/neodgm.ttf"
+
+menuFont = pygame.font.Font(fontPath, 50)
+subMenuFont = pygame.font.Font(fontPath, 20)
+
+menuText = menuFont.render("잠만보 먹방", True, (255, 255, 255))
+subMenuText = subMenuFont.render("엔터키 눌러서 시작하기!", True, (255, 255, 255))
+
+selectIndex = 0
+
+def menuTextChange():
+    global menuText
+    if selectIndex == 0:
+        menuText = menuFont.render("잠만보 먹방", True, (255, 255, 255))
+    elif selectIndex == 1:
+        menuText = menuFont.render("피카츄 배구", True, (255, 255, 255))
+    elif selectIndex == 2:
+        menuText = menuFont.render("꼬부기 살아남기", True, (255, 255, 255))
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                if selectIndex > 0:
+                    selectIndex -= 1
+                    menuTextChange()
+
+            elif event.key == pygame.K_RIGHT:
+                if selectIndex < 2:
+                    selectIndex += 1
+                    menuTextChange()
+
+        elif event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))  
+
+    menuTextRect = menuText.get_rect(center=(size_x // 2, size_y // 2))
+    subMenuTextRect = subMenuText.get_rect(center=(size_x // 2, size_y // 2 + 36))
+
+    screen.blit(menuText, menuTextRect.topleft)  
+    screen.blit(subMenuText, subMenuTextRect.topleft)  
+    
+    pygame.display.update()
